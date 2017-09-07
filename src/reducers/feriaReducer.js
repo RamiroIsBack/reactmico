5d5361f9d8dc52a3b8adfbf8eb5fc299ef12e1c2
Ramiro <ramiro.santamaria.alvarez@gmail.com>
2017-09-07 15:12:53 -0400

    un par de retokes visuales y arreglar las fechas de ferias joder con heroku! probando a subir a git primero a ver

--- src/reducers/feriaReducer.js
+++ src/reducers/feriaReducer.js
@@ -26,6 +26,7 @@ export default (state = initialState, action) => {
         return a[i]>b[i] ? 1 : a[i]<b[i] ? -1 : 0
       })
     }
+
     //las ferias mas nuevas abajo (si cambio el 1 y el -1 seria al reves)
 
     //lo paso a formato yyyy/mm/dd para poder operar con Date Object y miro a ver si est'a en curso o est'a caducada
@@ -35,9 +36,13 @@ export default (state = initialState, action) => {
       //                       year        month         day
       let feriaDate = new Date(parts[2], parts[1] - 1, parts[0])
 
+      //no lo uso aqui ahora pero as'i seria copiar un objeto
+      //let copyList = Object.assign([],this.state.list)
+
       //calculo el final de la feria segun sus dias d duracion
-      let finalFeriaDate = feriaDate
-      finalFeriaDate = new Date(feriaDate.getFullYear(),feriaDate.getMonth(),feriaDate.getDate()+list[i].duracion)
+      let finalFeriaDate =  new Date(parts[2], parts[1] - 1, parts[0])
+      let duracion = list[i].duracion
+      finalFeriaDate.setTime( finalFeriaDate.getTime() + duracion * 86400000 )
       list[i].fechaFinal = finalFeriaDate
       let diaHoy = new Date()
 
