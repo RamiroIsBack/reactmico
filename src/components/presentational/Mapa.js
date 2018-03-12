6782fbb42124328289745a6e9daf6cd27ba22f89
Ramiro <ramiro.santamaria.alvarez@gmail.com>
2018-03-12 09:41:02 -0400

    map style

--- src/components/presentational/Mapa.js
+++ src/components/presentational/Mapa.js
@@ -86,7 +86,123 @@ class Mapa extends Component {
     return (
 
       <GoogleMap
-
+        defaultOptions={{styles : [
+          {
+            'featureType': 'road',
+            'elementType': 'labels',
+            'stylers': [
+              {
+                'visibility': 'on'
+              }
+            ]
+          },
+          {
+            'featureType': 'poi',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'featureType': 'administrative',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'featureType': 'road',
+            'elementType': 'geometry.fill',
+            'stylers': [
+              {
+                'color': '#000000'
+              },
+              {
+                'weight': 1
+              }
+            ]
+          },
+          {
+            'featureType': 'road',
+            'elementType': 'geometry.stroke',
+            'stylers': [
+              {
+                'color': '#000000'
+              },
+              {
+                'weight': 0.8
+              }
+            ]
+          },
+          {
+            'featureType': 'landscape',
+            'stylers': [
+              {
+                'color': '#ffffff'
+              }
+            ]
+          },
+          {
+            'featureType': 'water',
+            'elementType': 'all',
+            'stylers': [
+              {
+                'color': '#e5fcfc'
+              }
+            ]
+          },
+          {
+            'featureType': 'transit',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'elementType': 'labels',
+            'stylers': [
+              {
+                'visibility': 'off'
+              }
+            ]
+          },
+          {
+            'elementType': 'labels.text',
+            'stylers': [
+              {
+                'visibility': 'on'
+              }
+            ]
+          },
+          {
+            'elementType': 'labels.text.stroke',
+            'stylers': [
+              {
+                'color': '#ffffff'
+              }
+            ]
+          },
+          {
+            'elementType': 'labels.text.fill',
+            'stylers': [
+              {
+                'color': '#000000'
+              }
+            ]
+          },
+          {
+            'elementType': 'labels.icon',
+            'stylers': [
+              {
+                'visibility': 'on'
+              }
+            ]
+          }
+        ]
+        }}
         defaultZoom={7}
         defaultCenter={{  lat: 43.0123411, lng: -7.5915196  }}
       >
